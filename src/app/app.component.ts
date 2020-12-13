c9a595c9e18a352fcaa24ddb6d0d0b4ed97085e7
BunyaminKucuk <BunyaminKucuk@github.com>
2020-12-13 17:58:30 +0300

    feat:desing app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,6 +14,7 @@ declare let pdfMake: any;
 export class AppComponent {
 
   resume = new Resume();
+  skill=new Skill()
   degrees = ['Primary School', 'Secondary School', 'High School', 'University'];
 
   constructor(private scriptService: ScriptService) {
